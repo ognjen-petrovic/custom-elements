79da2dc56760b078b529c0fdbf09ec6f120a3a86
Ognjen <ognjen@heliozgroup.com>
2018-11-20 15:15:24 +0100

    Work in progress...

--- elements/hz-img-magnifier-glass/hz-img-magnifier-glass.js
+++ elements/hz-img-magnifier-glass/hz-img-magnifier-glass.js
@@ -5,22 +5,9 @@ customElements.define('hz-img-magnifier-glass',
       constructor() {
         super();
 
-        var img, glass, w, h, bw;
-        /*
-        img = document.getElementById(imgID);
-        glass = document.createElement("div");
-        glass.setAttribute("class", "img-magnifier-glass");
-        img.parentElement.insertBefore(glass, img);
-        glass.style.backgroundImage = "url('" + img.src + "')";
-        glass.style.backgroundRepeat = "no-repeat";
-        console.log(img.offsetHeight);
-        glass.style.backgroundSize = (img.offsetWidth * zoom) + "px " + (img.offsetHeight * zoom) + "px";
-        bw = 3;
-        w = glass.offsetWidth / 2;
-        h = glass.offsetHeight / 2;
-*/
         this.style.display= 'block';
         this.style.position = 'relative';
+
         this.glass = document.createElement("div");
         Object.assign(this.glass.style, {
             position: 'absolute',
@@ -29,6 +16,7 @@ customElements.define('hz-img-magnifier-glass',
         })
 
         this._bw = 3;
+        this._zoom = this.zoom;
 
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(this.glass);
@@ -40,25 +28,24 @@ customElements.define('hz-img-magnifier-glass',
 
         this.glass.addEventListener("mousemove", e => this._moveMagnifier(e));
         this.img.addEventListener("mousemove", e => this._moveMagnifier(e));
+        /*and also for touch screens:*/
+        //this.glass.addEventListener("touchmove", moveMagnifier);
+        //this.img.addEventListener("touchmove", moveMagnifier);
 
         this.glass.style.backgroundImage = "url('" + this.img.src + "')";
         this.glass.style.backgroundSize = (this.img.offsetWidth * this.zoom) + "px " //+ (this.img.offsetHeight * this.zoom);
+
         Object.assign(this.glass.style, {
             width:  this.size,
             height: this.size,
-            border: '3px solid ' + this.color,
-            'border-radius': '50%',
+            border: this.border,
+            'border-radius': this.borderRadius,
         })
-        this.w = this.glass.offsetWidth / 2;
-        this.h = this.glass.offsetHeight / 2;
+        this._w = this.glass.offsetWidth / 2;
+        this._h = this.glass.offsetHeight / 2;
     };
 
     _moveMagnifier(e) {
-        var w = this.w;
-        var img = this.img;
-        var zoom = this.zoom;
-        var h = this.h;
-        var glass = this.glass;
         var pos, x, y;
         /*prevent any other actions that may occur when moving over the image*/
         e.preventDefault();
@@ -67,23 +54,22 @@ customElements.define('hz-img-magnifier-glass',
         x = pos.x;
         y = pos.y;
         /*prevent the magnifier glass from being positioned outside the image:*/
-        if (x > img.offsetWidth - (w / zoom)) { x = img.offsetWidth - (w / zoom); }
-        if (x < w / zoom) { x = w / zoom; }
-        if (y > img.offsetHeight - (h / zoom)) { y = img.offsetHeight - (h / zoom); }
-        if (y < h / zoom) { y = h / zoom; }
+        if (x > this.img.offsetWidth - (this._w / this._zoom)) { x = this.img.offsetWidth - (this._w / this._zoom); }
+        if (x < this._w / this._zoom) { x = this._w / this._zoom; }
+        if (y > this.img.offsetHeight - (this._h / this._zoom)) { y = this.img.offsetHeight - (this._h / this._zoom); }
+        if (y < this._h / this._zoom) { y = this._h / this._zoom; }
         /*set the position of the magnifier glass:*/
-        glass.style.left = (x - w) + "px";
-        glass.style.top = (y - h) + "px";
+        this.glass.style.left = (x - this._w) + "px";
+        this.glass.style.top = (y - this._h) + "px";
         /*display what the magnifier glass "sees":*/
-        glass.style.backgroundPosition = "-" + ((x * zoom) - w + this._bw) + "px -" + ((y * zoom) - h + this._bw) + "px";
+        this.glass.style.backgroundPosition = "-" + ((x * this._zoom) - this._w + this._bw) + "px -" + ((y * this._zoom) - this._h + this._bw) + "px";
     }
 
     _getCursorPos(e) {
-        var img = this.img;
         var a, x = 0, y = 0;
         e = e || window.event;
         /*get the x and y positions of the image:*/
-        a = img.getBoundingClientRect();
+        a = this.img.getBoundingClientRect();
         /*calculate the cursor's x and y coordinates, relative to the image:*/
         x = e.pageX - a.left;
         y = e.pageY - a.top;
@@ -93,20 +79,20 @@ customElements.define('hz-img-magnifier-glass',
         return { x: x, y: y };
     }
 
-    get color() {
-        return this._getAttributeAsProperty('color', 'black');
+    get borderRadius() {
+        return this._getAttributeAsProperty('border-radius', '50%');
     }
 
-    get size()
-    {
+    get border() {
+        return this._getAttributeAsProperty('border', '3px solid black');
+    }
+
+    get size() {
         return this._getAttributeAsProperty('size', this.img.offsetWidth/3 + 'px');
-
     }
 
-    get zoom()
-    {
+    get zoom() {
         return this._getAttributeAsProperty('zoom', 3);
-
     }
 
     _getAttributeAsProperty(propertyName, defaultPropertyValue) {
